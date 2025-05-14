3e57276b1ac8e69c2b2c7e635d9ef73902d5f14d
shs332 <shs346752@kakao.com>
2025-05-15 01:30:11 +0900

    .

---
+++ link_data.sh
@@ -0,0 +1,4 @@
+ln -s /root/wlsgur4011/GESI/gsplat/data/DFA data/
+ln -s /root/wlsgur4011/GESI/gsplat/data/DFA_processed data/
+ln -s /root/wlsgur4011/GESI/gsplat/data/Diva360_data data/
+ln -s /root/wlsgur4011/GESI/gsplat/data/Diva360_processed data/
